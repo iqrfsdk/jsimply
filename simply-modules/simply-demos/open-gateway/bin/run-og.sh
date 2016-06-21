5b2a9c195d6e1a535ba374abb788f30c59f6ba89
Rostislav Spinar <rostislav.spinar@microrisc.com>
2016-06-21 15:51:18 +0200

    OG bin update

--- simply-modules/simply-demos/open-gateway/bin/run-og.sh
+++ simply-modules/simply-demos/open-gateway/bin/run-og.sh
@@ -2,6 +2,6 @@
 
 sudo /usr/bin/java -Djava.library.path=natives/armv7l \
 	-Dlogback.configurationFile=config/logback/logback.xml \
-	-cp open-gateway-0.0.1.jar: \
+	-cp open-gateway-0.0.2.jar: \
 	com.microrisc.opengateway.OpenGateway
 #	com.microrisc.opengateway.OpenGateway > og.log 2>&1
