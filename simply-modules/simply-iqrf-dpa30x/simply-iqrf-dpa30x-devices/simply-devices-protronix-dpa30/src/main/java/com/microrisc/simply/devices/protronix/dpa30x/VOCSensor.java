c958c1c664655f0e0cf18edbc419c6b669d007ec
Michal Konopa <michal.konopa@microrisc.com>
2017-03-22 11:35:51 +0100

    Simply IQRF DPA: ---------------- - renamed some directories(modules) - added autonetwork into demos - added protronix into devices

---
+++ simply-modules/simply-iqrf-dpa30x/simply-iqrf-dpa30x-devices/simply-devices-protronix-dpa30/src/main/java/com/microrisc/simply/devices/protronix/dpa30x/VOCSensor.java
@@ -0,0 +1,46 @@
+/* 
+ * Copyright 2016 MICRORISC s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.microrisc.simply.devices.protronix.dpa30x;
+
+import com.microrisc.simply.DeviceInterface;
+import com.microrisc.simply.di_services.CallErrorsService_sync;
+import com.microrisc.simply.di_services.CallRequestProcessingService_sync;
+import com.microrisc.simply.di_services.WaitingTimeoutService;
+import com.microrisc.simply.iqrf.dpa.v30x.di_services.DPA_AdditionalInfoService;
+import com.microrisc.simply.devices.protronix.dpa30x.types.VOCSensorData;
+
+/**
+ * VoC Sensor Device Interface.
+ * <p>
+ * IMPORTANT NOTE: <br>
+ * Every method returns {@code NULL}, if an error has occurred during processing
+ * of this method.
+ * 
+ * @author Michal Konopa
+ */
+@DeviceInterface
+public interface VOCSensor
+extends WaitingTimeoutService, CallRequestProcessingService_sync, 
+        CallErrorsService_sync, DPA_AdditionalInfoService 
+{
+    
+    /**
+     * Returns data from the sensor.
+     * @return sensor data <br>
+     *         {@code null}, if an error has occurred during processing
+     */
+    VOCSensorData get();
+}
